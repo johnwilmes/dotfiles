dbfd3da14a3615c88c42b07d4353dd57e3bcba04
John Wilmes <jw@johnwilmes.name>
2024-11-20 14:36:10 -0600

    Disable bell in wezterm

--- wezterm.lua
+++ wezterm.lua
@@ -1,7 +1,8 @@
 local wezterm = require('wezterm')
-local config = {}
+local config = wezterm.config_builder()
 
 config.enable_kitty_keyboard = true
 config.use_dead_keys = false
+config.audible_bell = "Disabled"
 
 return config
